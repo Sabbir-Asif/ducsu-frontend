08d5440f8a6a006f95a665e08999237dee6bc790
Sabbir_Asif <113287134+Sabbir-Asif@users.noreply.github.com>
2025-09-01 20:33:05 +0600

    chore: setup next and ant design (#1)

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from 'antd';
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      <Button type='primary'>Hello World</Button>
+    </div>
+  );
+};
+
+export default page;
